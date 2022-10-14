2f733d8483a278c9844219760403f899bf27e520
Admin <Admin@DESKTOP-57MEOSK>
2022-10-14 14:02:22 -0600

    Agregar archivos de proyecto.

---
+++ L11_RAJCL_1073522/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        int Moneda;
+        double Monto;
+        Console.WriteLine("Convertidor de monedas a Euros");
+        Console.WriteLine("Para convertir de libras ingrese 1, dolares ingrese 2 y yenes ingrese 3");
+        Moneda = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese la cantidad a convertir");
+
+        Monto = Convert.ToDouble(Console.ReadLine());
+        
+        double resultado = conversionMoneda(Moneda, Monto);
+        Console.WriteLine("Su cantidad en Euros es: " + resultado);
+ 
+    }
+
+
+    private static double conversionMoneda(int Moneda, double Monto)
+    {
+  
+        if (Moneda == 1)
+        {
+            return Monto * 1.22;
+        }
+
+    
+        if (Moneda == 2)
+        {
+            return Monto * 0.75;
+        }
+
+        if (Moneda == 3)    
+        {
+            return Monto * 0.009;
+        }
+
+        return 0;
+    }
+}
